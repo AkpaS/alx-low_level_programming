777ac4516d59a4ed96ce708bb46e428418b51c1d
Akpa Sophie <akpasophie@gmail.com>
2022-06-01 15:22:13 -0700

    Created_a_cFile_that_prints_using_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
+
 /**
- *
  * main - Entry point
- * this is a file that uses puts instead of printf
- * 
- * Return: Always 0 (Success)
  *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
