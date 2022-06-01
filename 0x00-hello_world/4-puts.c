ad5f58f6dcec56e7416a2eb237611cfb45f33715
Akpa Sophie <akpasophie@gmail.com>
2022-06-01 15:01:59 -0700

    Created_a_cFile_that_prints_using_puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * this is a file that uses puts instead of printf
+ *
+*/
+
+int main(void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+	return 0;
+}
