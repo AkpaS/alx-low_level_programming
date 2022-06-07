59203834978c6d0d257481bab13cf83cb574500f
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 13:04:40 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Returns values for many arguments
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
