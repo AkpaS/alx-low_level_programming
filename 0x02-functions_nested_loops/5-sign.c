fe4d7210470c38bf6a59ecc229af9e50989b4f1b
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 15:19:10 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,16 +3,16 @@
  * print_sign - finds <,> and =
  * @n: integer
  *
- * Return: 0 when > 0, 0 when = 0 and -1 when < 0
+ * Return: 1 when > 0, 0 when = 0 and -1 when < 0
  */
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	if (n == 48)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
