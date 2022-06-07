1d7336de32eddac7379adefd0f81aedae9681815
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 13:33:41 -0700

    isLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Returns values thaat are lowercase
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+
+	if (c < 99)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
