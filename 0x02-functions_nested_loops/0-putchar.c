1b15e0f84a232792c45010a7faba68f4db965a62
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 08:18:03 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Hello there
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[10] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
