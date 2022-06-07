732c98f5aeaba9e875feae8c42ce7bf49145d42b
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 15:46:56 -0700

    haha

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,11 @@
 #include "main.h"
 /**
  * print_sign - finds <,> and =
- * @n: integer
+ * @x: integer
  *
  * Return: 1 when > 0, 0 when = 0 and -1 when < 0
  */
-int print_sign(int n)
+int print_sign(int x)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	return (abs(x));
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @y: the number
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int y)
+{
+	int x;
+
+	x = y % 10;
+	if (x < 0)
+		x = x * -1;
+	_putchar('0' + 1);
+	return (1);
+}
