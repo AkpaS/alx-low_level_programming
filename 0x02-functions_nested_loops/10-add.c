a54aa1eb3c4e5963e78dfc9f861e6311e5b2a1a7
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 17:19:23 -0700

    a+b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,10 +3,13 @@
 /**
  * add - adds two integers and returns the result
  *
+ * @y: the integer y
+ * @x: the integer x
  * Return: returns result
  */
 int add(int x, int y)
 {
 	int sum = y + x;
+
 	return (sum);
 }
