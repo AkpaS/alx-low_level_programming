9283ab6f1244ad75dd05200b2f783c804b9bf106
Akpa Sophie <akpasophie@gmail.com>
2022-06-07 18:57:12 -0700

    fib2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int x, y = 1, z = 2, add = 0, Sadd = 0;
+
+	for (x = 0, x < 49; x++)
+	{
+		if ((z % 2 == 0) && (z <= 4000000))
+		{
+			Sadd = Sadd + z;
+		}
+		add = y + z;
+		y = z;
+		z = add;
+	}
+	printf("ld\n", Sadd);
+	return (0);
+}
