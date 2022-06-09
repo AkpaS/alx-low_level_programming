a7ea93b53ad1c8ebdc02d555fec3d6fda33ec213
Akpa Sophie <akpasophie@gmail.com>
2022-06-09 07:43:40 -0700

    numb

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: 0 or 1
+ */
+
+void more_numbers(void)
+{
+	int x, more;
+
+	for (more = 0; more <= 9; more++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar((x / more) + '0');
+		}
+		_putchar((x % 10) + '0');
+	}
+	_putchar('\n');
+}
