c24af93b347b09995092fbfbe8c7ff96a369002c
Akpa Sophie <akpasophie@gmail.com>
2022-06-09 18:10:08 -0700

    should have seen the sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,5 +7,19 @@
  */
 int print_sign(int x)
 {
-	return (abs(x));
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1)
+	}
 }
