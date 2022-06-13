f01917933ed00c4312c681c793d711cd096c59f8
Sophie Akpa <40465811+AkpaS@users.noreply.github.com>
2022-06-13 11:54:49 +0100

    98_Battery_str

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes a pointer and updates the value it points
  * @n: this is the pointer
- * 
  * Return: 0 or 1
  */
 
