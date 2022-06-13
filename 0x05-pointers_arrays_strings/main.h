1652360a51f514f61fe918cbb76149f388fb4505
Sophie Akpa <40465811+AkpaS@users.noreply.github.com>
2022-06-13 11:52:27 +0100

    98_Battery_str

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Abinet Tesfu
+ * Desc: Header file containing declarations for all functions
+ *
+ */
+int main(void);
+int _putchar(char);
+void reset_to_98(int *n);
+
+#endif
