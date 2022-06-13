eb301f41b9b4ad204b7ab32bdb21676a387454eb
Akpa Sophie <akpasophie@gmail.com>
2022-06-13 12:24:29 -0700

    98_Battery_str

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,13 @@
 
 /**
  * reset_to_98 - takes a pointer and updates the value it points
+ *
  * @n: this is the pointer
+ *
  * Return: 0 or 1
  */
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98:
 }
