d0a60d6c13db453f97f95c3918d006fde6542064
Akpa Sophie <akpasophie@gmail.com>
2022-06-13 17:48:15 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _atoi - converts a string to integer
  *
@@ -7,33 +6,21 @@
  *
  * Return: 0 or 1
  */
-
 int _atoi(char *s)
 {
-	int a;
-	int b;
-	int c;
-	int x;
-	int y;
-	int z;
-
-	a = 0;
-	b = 0;
-	c = 0;
-	x = 0;
-	y = 0;
-	z = 0;
-
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
 
 	while (s[x] != '\0')
 		x++;
 	while (a < x && y == 0)
 	{
 		if (s[a] == '-')
-		{
 			++b;
-		}
-
 		if (s[a] >= '0' && s[a] <= '9')
 		{
 			z = s[a] - '0';
@@ -47,7 +34,6 @@ int _atoi(char *s)
 		}
 		a++;
 	}
-
 	if (y == 0)
 		return (0);
 	return (c);
