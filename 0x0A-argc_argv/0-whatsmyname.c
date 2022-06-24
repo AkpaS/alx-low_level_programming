030aa9d0ee739c10da0addd7c8591bf2ad4e67cb
Akpa Sophie <akpasophie@gmail.com>
2022-06-24 06:13:56 -0700

    WhatIsMyName

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - writes a program that prints its name
+ *
+ * @argc: argument count
+ * @argv: array of argument strings
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return(0);
+}
