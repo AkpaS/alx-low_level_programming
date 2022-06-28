488f9833cba9187f6b0037d4d5d558398084bc6d
Akpa Sophie <akpasophie@gmail.com>
2022-06-28 14:12:25 -0700

    Segementation_dump

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * word_count - checks the num of words in a string
+ *
+ * @x: the string pointer
+ *
+ * Return: Num of words in a string
+ */
+
+int word_count(char *x)
+{
+	int sign;
+	int y;
+	int z;
+
+	sign = 0;
+	z = 0;
+
+	for (y = 0; x[y] != '\0'; y++)
+	{
+		if (x[y] == ' ')
+		{
+			sign = 0;
+		}
+		else if (sign == 0)
+		{
+			sign = 1;
+			z++;
+		}
+	}
+	return (z);
+}
+
+
+/**
+ * strtow - A func that splits a string into two
+ *
+ * @str: the string pointer
+ *
+ * Return: NULL if the func fails  or a pointer to an array ot str
+ */
+
+char **strtow(char *str)
+{
+	char **grid;
+	char *f;
+	int a, b = 0, c = 0, word, rg = 0, dy, dx;
+
+	while (*(str + rg))
+		rg++;
+	word = word_count(str);
+	if (word == 0)
+		return (NULL);
+	grid = (char **) malloc(sizeof(char *) * (word + 1));
+	if (grid == NULL)
+		return (NULL);
+	for (a = 0; a <= rg; a++)
+	{
+		if (str[a] == ' ' || str[a] == '\0')
+		{
+			if (c)
+			{
+				dx = a;
+				f = (char *) malloc(sizeof(char) * (c + 1));
+				if (f == NULL)
+					return (NULL);
+				while (dy < dx)
+					*f++ = str[dy++];
+				*f = '\0';
+				grid[b] = f - c;
+				b++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			dy = a;
+	}
+	grid[b] = NULL;
+	return (grid);
+}
