ea551fbe76d3abb425d1dc3254c72c996b8e1f1b
Sophie Akpa <akpasophie@gmail.com>
2022-07-19 19:56:43 -0700

    list_lenght

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,6 +4,8 @@
  * listint_len - A func that returns the number of elements in a linked list
  *
  * @h: the pointer to the head node of the linked list
+ *
+ * Return: the number of elements in a linked list
  */
 
 size_t listint_len(const listint_t *h)
